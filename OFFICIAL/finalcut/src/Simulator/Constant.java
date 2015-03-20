870e9885d52af827e5b68e1451d684b87a52455a
Sim Hong Xun <sim.hongxun@gmail.com>
2015-03-21 04:25:36 +0800

    Version 0.1

---
+++ OFFICIAL/finalcut/src/Simulator/Constant.java
@@ -0,0 +1,56 @@
+package Simulator;
+
+public class Constant {
+
+	public static int starthour =0;
+	public static int startminute =0;
+	
+	public static int endday =0;
+	
+	public static int endhour =0;
+	public static int endminute =0;
+	
+	public static int CDCstarth =0;
+	public static int CDCstartm =0;
+	public static int CDCendh =0;
+	public static int CDCendm =0;
+	public static int CDCcargoto =0;
+	public static int CDCbay =0;
+	public static int CDCdemandh[] = new int[15];
+	
+	
+	public static int Factstarth =0;
+	public static int Factstartm =0;
+	public static int Factendh =0;
+	public static int Factendm =0;
+	public static int Factcargoto =0;
+	public static int Factbay =0;
+	public static int Factdemandh[] = new int[16];
+	
+	
+	
+	
+	public static boolean TruckMode;
+	public static int TruckQty;
+	public static double TruckSpd;
+	public static double TruckAccSpd;
+	public static double TruckDecSpd;
+	public static double TruckTimeGap;
+	public static double TruckDistGap;
+	public static double TruckPoliteness;
+	public static double TruckChanging;
+	
+	public static boolean AgvMode;
+	public static int AgvQty;
+	public static double AgvSpd;
+	public static double AgvAccSpd;
+	public static double AgvDecSpd;
+	public static double AgvTimeGap;
+	public static double AgvDistGap;
+	
+	static final double TIMESTEP_S = 0.2;   // simulation time step
+	public static int tsleep_ms = 20;
+	
+
+	
+}
